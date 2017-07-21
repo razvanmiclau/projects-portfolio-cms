99a7680670841a5b474fc8c29f82130003f14312
razvanmiclau <razvan.miclau@gmail.com>
2017-07-21 14:26:57 +0100

    Uppercase Test

--- client/src/components/User/UI/Project.jsx
+++ client/src/components/User/UI/Project.jsx
@@ -13,7 +13,7 @@ export default class Project extends PureComponent {
         </a>
         <div className="caption">
           <h3>{project_name}</h3>
-          <p>{project_desc.substring(this,0,20)}</p>
+          <p>{project_desc.uppercase()}</p>
           <ul className="actions">
             <li><span className="button small">Details</span></li>
           </ul>
